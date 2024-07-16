f8f3fe9490d43bd6a7942098c13d64539d98f8a2
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-07-16 10:32:30 +0700

    feat: :sparkles: base layout starter template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 export default function Home() {
 	return (
 		<main>
-			<article className="prose lg:prose-lg">
-				<h1>H1</h1>
-				<h2>H2</h2>
-				<h3>H3</h3>
-				<h4>H4</h4>
-				<h5>H5</h5>
-				<p>Test</p>
+			<article className="prose lg:prose-lg max-w-none">
+				<h1>Hello world!</h1>
+				<code>
+					This is just a starter template, made using Next.js app dir +
+					Typescript + Tailwind CSS (with Daisy UI).
+				</code>
+				<div className="mt-12 flex w-full items-center justify-center">
+					<a
+						href="https://github.com/new?template_name=next14-starter&template_owner=yehezkielgunawan"
+						target="_blank"
+						className="btn"
+						rel="noreferrer"
+					>
+						Generate Project using this template
+					</a>
+				</div>
 			</article>
 		</main>
 	);
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 import useIsDark from "@/hooks/useIsDark";
+import { FaGithub } from "react-icons/fa";
 import { IoMoon, IoSunny } from "react-icons/io5";
 
 const Header = () => {
@@ -7,39 +8,38 @@ const Header = () => {
 
 	return (
 		<header className="bg-base-200">
-			<nav className="navbar mx-auto max-w-4xl px-2 md:px-0">
-				<div className="flex-1">
-					<a href="/">NextJS 14 Template</a>
-				</div>
-				<div className="flex-none">
-					<ul className="menu menu-horizontal">
-						<li>
-							<details>
-								<summary>Parent</summary>
-								<ul className="rounded-t-none bg-base-100 p-2">
-									<li>
-										<a href="/">Link 1</a>
-									</li>
-									<li>
-										<a href="/">Link 2</a>
-									</li>
-								</ul>
-							</details>
-						</li>
-						<li>
-							<label className="swap swap-rotate">
-								<input
-									type="checkbox"
-									className="theme-controller"
-									value="dim"
-									onChange={(e) => setIsDark(e.target.checked)}
-								/>
+			<nav className="container mx-auto">
+				<div className="navbar px-0">
+					<div className="flex-1">
+						<a href="/">NextJS 14 Template</a>
+					</div>
+					<div className="flex-none">
+						<ul className="menu menu-horizontal">
+							<li>
+								<a
+									href="https://github.com/yehezkielgunawan/next14-starter"
+									target="_blank"
+									className="btn btn-ghost btn-sm"
+									rel="noreferrer"
+								>
+									<FaGithub size={20} />
+								</a>
+							</li>
+							<li>
+								<label className="swap swap-rotate btn btn-ghost btn-sm">
+									<input
+										type="checkbox"
+										className="theme-controller"
+										value="dim"
+										onChange={(e) => setIsDark(e.target.checked)}
+									/>
 
-								<IoSunny className="swap-off h-5 w-5" />
-								<IoMoon className="swap-on h-5 w-5" />
-							</label>
-						</li>
-					</ul>
+									<IoSunny size={20} className="swap-off" />
+									<IoMoon size={20} className="swap-on" />
+								</label>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</nav>
 		</header>
