67c6d1244297e210cdfa1fbe7fe3fb97328998f6
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-08-07 09:41:45 +0700

    refactor(GeneralWrapper.tsx): :recycle: use GeneralWrapper as reusable wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import { ThemeProvider } from "@/context/ThemeProvider";
 import { metadataContent } from "@/services/metadata";
 
@@ -27,9 +25,7 @@ export default function RootLayout({
 					enableSystem
 					themes={["nord", "dim"]}
 				>
-					<Header />
 					{children}
-					<Footer />
 				</ThemeProvider>
 			</body>
 		</html>
